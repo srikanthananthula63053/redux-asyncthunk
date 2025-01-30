f0439c2a65f4099e8e4cf2f9883e791d1b5033c8
srikanthananthula63053 <sreekanthananthula5@gmail.com>
2025-01-30 16:41:39 +0530

    creating async thunk

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,51 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from 'date-fns';
+
+const initialState = {
+    posts: [],
+    status: 'idle', //idle | loading | succeded| failed
+    error: null
+}
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers: {
+        postAdded: {
+            reducer(state, action) {
+                state.push(action.payload)
+            },
+            prepare(title, content, userId) {
+                return {
+                    payload: {
+                        id: nanoid(),
+                        title,
+                        content,
+                        date: new Date().toISOString(),
+                        userId,
+                        reactions: {
+                            thumbsUp: 0,
+                            wow: 0,
+                            heart: 0,
+                            rocket: 0,
+                            coffee: 0
+                        }
+                    }
+                }
+            }
+        },
+        reactionAdded(state, action) {
+            const { postId, reaction } = action.payload
+            const existingPost = state.find(post => post.id === postId)
+            if (existingPost) {
+                existingPost.reactions[reaction]++
+            }
+        }
+    }
+})
+
+export const selectAllPosts = (state) => state.posts;
+
+export const { postAdded, reactionAdded } = postsSlice.actions
+
+export default postsSlice.reducer
